1a023b60206cbab197682df25d8ec07b418770d1
alags <alagus@gmail.com>
2023-09-09 17:31:58 +0000

    use static content

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ let app = express();
 
 app.get('/', (req, res) => {
     absolutePath = __dirname;
+    app.use('/public/', express.static(absolutePath + '/public'));
     res.sendFile(absolutePath + '/views/index.html');
 });
 
