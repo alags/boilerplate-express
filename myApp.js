e694139787ec21c2e32e28c53d442b519eb53692
alags <alagus@gmail.com>
2023-09-09 17:04:57 +0000

    Add static content

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 
+console.log('Hello World');
 
-
-
-
-
-
+app.get('/', (req, res) => {
+    absoluteStaticPath = __dirname + '/public';
+    app.use(express.static(absoluteStaticPath));
+    });
 
 
 
